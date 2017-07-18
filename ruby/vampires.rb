34f6bcd6a33fe9f0b0493596d375237f2035af89
Nicole Cody <nicole.cody7@gmail.com>
2017-07-18 11:33:03 -0500

    Add age/garlic bread/insurance qualifier

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,14 @@
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-year_born = gets.chomp
+year_born = gets.chomp.to_i
 puts "Our company cafetia serves garlic bread. Should we order some for you?"
 garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
 
+if age == (2017 - year_born) && (garlic_bread || insurance == yes)
+  puts "Probably not a vampire."
+end

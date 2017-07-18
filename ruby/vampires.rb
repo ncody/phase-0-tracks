58f692050f0bf75c6e689d1143ff3e3b73607fc0
Nicole Cody <nicole.cody7@gmail.com>
2017-07-18 13:01:10 -0500

    Add definitely a vampire and results inconclusive and downcase

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,18 +6,26 @@ puts "What year were you born?"
 year_born = gets.chomp.to_i
 puts "Our company cafetia serves garlic bread. Should we order some for you?"
 garlic_bread = gets.chomp
+garlic_bread.downcase!
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+insurance.downcase!
 
-if age == (2017 - year_born) && (garlic_bread == "yes" || insurance == "yes")
+
+if name == "Tu Fang" || name =="Drake Cula"
+  puts "Defintely a vampire."
+
+elsif age == (2017 - year_born) && (garlic_bread == "yes" || insurance == "yes")
   puts "Probably not a vampire."
 
-elsif age != (2017 - year_born) && (garlic_bread == "no" && insurance == "no")
+elsif age != (2017 - year_born) && garlic_bread == "no" && insurance == "no"
   puts "Almost certainly a vampire."
 
 elsif age != (2017 - year_born) && (garlic_bread == "no" || insurance == "no")
   puts "Probably a vampire."
 
+else
+  puts "Results inconclusive."
 
 end
 

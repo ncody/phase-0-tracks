e500d60ecec4065129962dbfc82ee79d0af4bbc2
Nicole Cody <nicole.cody7@gmail.com>
2017-07-18 13:16:16 -0500

    Add loop for number of employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,31 +1,38 @@
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-year_born = gets.chomp.to_i
-puts "Our company cafetia serves garlic bread. Should we order some for you?"
-garlic_bread = gets.chomp
-garlic_bread.downcase!
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
-insurance.downcase!
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+
+i = 0
+while i < employees do
+  puts "What is your name?"
+  name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  puts "What year were you born?"
+  year_born = gets.chomp.to_i
+  puts "Our company cafetia serves garlic bread. Should we order some for you?"
+  garlic_bread = gets.chomp
+  garlic_bread.downcase!
+  puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+  insurance.downcase!
 
 
-if name == "Tu Fang" || name =="Drake Cula"
-  puts "Defintely a vampire."
+  if name == "Tu Fang" || name =="Drake Cula"
+    puts "Defintely a vampire."
 
-elsif age == (2017 - year_born) && (garlic_bread == "yes" || insurance == "yes")
-  puts "Probably not a vampire."
+  elsif age == (2017 - year_born) && (garlic_bread == "yes" || insurance == "yes")
+    puts "Probably not a vampire."
 
-elsif age != (2017 - year_born) && garlic_bread == "no" && insurance == "no"
-  puts "Almost certainly a vampire."
+  elsif age != (2017 - year_born) && garlic_bread == "no" && insurance == "no"
+    puts "Almost certainly a vampire."
 
-elsif age != (2017 - year_born) && (garlic_bread == "no" || insurance == "no")
-  puts "Probably a vampire."
+  elsif age != (2017 - year_born) && (garlic_bread == "no" || insurance == "no")
+    puts "Probably a vampire."
 
-else
-  puts "Results inconclusive."
+  else
+    puts "Results inconclusive."
 
+  end
+  i = i + 1
 end
 

60d12bbd5e08a7dae3cf9df9529f5f5baa7df9c4
Nicole Cody <nicole.cody7@gmail.com>
2017-07-18 10:53:35 -0500

    Add questions with variables for answers about vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year_born = gets.chomp
+puts "Our company cafetia serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+

720f81f640f22c165ece27011f63e7abd4169ab6
Nicole Cody <nicole.cody7@gmail.com>
2017-07-26 09:38:51 -0500

    Add most methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,35 @@
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".replace "zoom"
+#OR "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".gsub(/$/, ' suspects')
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".insert(0, 'the usual ')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".split.join (" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+# "z".ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+#122 is the ASCII number (American Standard Code for Information Interchange). ASCII associates an integer with each character (letters, punctuation, etc.) This ensures that different devices can communicate with each other despite differernces in character-sets.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count "4"
 # => 4

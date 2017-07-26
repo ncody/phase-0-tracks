95146392fab4d01a341c215a17e73e5b96901c52
Nicole Cody <nicole.cody7@gmail.com>
2017-07-26 13:29:42 -0500

    Add all methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,45 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".replace "zoom"
-#OR "zom".insert(1, 'o')
+puts "zom".replace "zoom"
+#OR
+puts "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".center(20)
+puts "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".gsub(/$/, ' suspects')
+puts "the usual".gsub(/$/, ' suspects')
+#OR
+puts "the usual".insert(-1, 'suspects')
 #=> "the usual suspects"
 
-# " suspects".insert(0, 'the usual ')
+puts " suspects".insert(0, 'the usual ')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".delete "T"
+puts "The mystery of the missing first letter".delete "T"
+#OR
+puts "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".split.join (" ")
+puts "Elementary,    my   dear        Watson!".split.join (" ")
+#OR
+puts "Elementary,    my   dear        Watson!".squeeze (" ")
 # => "Elementary, my dear Watson!"
 
-# "z".ord
+puts "z".ord
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
 #122 is the ASCII number (American Standard Code for Information Interchange). ASCII associates an integer with each character (letters, punctuation, etc.) This ensures that different devices can communicate with each other despite differernces in character-sets.
 
-# "How many times does the letter 'a' appear in this string?".count "4"
+puts "How many times does the letter 'a' appear in this string?".count "4"
 # => 4

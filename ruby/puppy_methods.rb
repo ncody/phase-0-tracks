6550ff6120cfd1367f7b609aadb023617aa08ee8
Nicole Cody <nicole.cody7@gmail.com>
2017-07-31 14:25:53 -0500

    Add methods for Puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    "Woof!" * num
+  end
+
+  def roll_over
+    "*rolls over*"
+  end
+
+  def dog_years(age)
+    age.to_i * 7
+  end
+
+  def sit
+    "*sits*"
+  end
 end
 
+spot = Puppy.new
+p spot.fetch("ball")
+p spot.speak(3)
+p spot.roll_over
+p spot.dog_years(3.5)
+p spot.dog_years(3)
+p spot.sit

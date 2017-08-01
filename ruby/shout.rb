333e26391f981ea9a872dd3b16aa340987f5e96e
Nicole Cody <nicole.cody7@gmail.com>
2017-08-01 10:59:17 -0500

    Add mixin of Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angirly(words)
     p words + "!!!" + ":("
@@ -10,3 +11,31 @@ end
 
 Shout.yell_angirly("no")
 Shout.yelling_happily("I am happy")
+=end
+
+
+module Shout
+  def yell_angirly(words)
+    p words + "!!!" + ":("
+  end
+
+  def yelling_happily(phrase)
+    p phrase + "!" + "YAY!" + ":)"
+  end
+end
+
+class People
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+people = People.new
+people.yell_angirly("hello")
+people.yelling_happily("hello")
+
+parrot = Parrot.new
+parrot.yell_angirly("no")
+parrot.yelling_happily("woo")

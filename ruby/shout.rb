a81ab1948a3536e076fcc6dc97f615caa6a07da1
Nicole Cody <nicole.cody7@gmail.com>
2017-08-01 10:39:18 -0500

    Add happy and angry yelling methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  #nothing for now
+  def self.yell_angirly(words)
+    p words + "!!!" + ":("
+  end
+
+  def self.yelling_happily(phrase)
+   p phrase + "!" + "YAY!" + ":)"
+  end
 end
+
+Shout.yell_angirly("no")
+Shout.yelling_happily("I am happy")

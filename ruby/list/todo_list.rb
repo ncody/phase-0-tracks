c315c95385c1c78423ec4f22fadbf41028f23713
Nicole Cody <nicole.cody7@gmail.com>
2017-08-01 12:24:59 -0500

    Add code to pass all test

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -7,4 +7,16 @@ class TodoList
   def get_items
    @chore
   end
+
+  def add_item(item)
+    @chore << item
+  end
+
+  def delete_item(item)
+    @chore.delete(item)
+  end
+
+  def get_item(index)
+    @chore[index]
+  end
 end

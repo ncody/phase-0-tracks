da0edc6c2a96459406e6924c779160533a4832ea
Nicole Cody <nicole.cody7@gmail.com>
2017-08-05 13:37:12 -0500

    Add test and code  to add guessed letter

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,8 +1,16 @@
 require_relative 'game'
 
 describe Game do
+=begin
+commented out because this method has to be in initialize in order to make an array of the hashes
   it "takes the word from initialize and outputs a string with hashes in place of every character" do
     game = Game.new('dog')
     expect(game.hash).to eq "___"
+  end
+=end
+
+  it "checks the original word for the letter, if it has it it adds it in that index to the array of hashes, if not, it adds the letter to incorrect guesses" do
+     game = Game.new('dog')
+     expect(game.check_word('d')).to eq ["d", "_", "_"]
   end
 end

0dc4a414fb3ea8aae22c6ec60956855d77ac95db
Nicole Cody <nicole.cody7@gmail.com>
2017-08-05 12:50:41 -0500

    Add test to make a word into ___ and code to solve it

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,28 +1,17 @@
-=begin
-create a class for hangman
-upon creation of a new instance, input a new word
-save this word and find the length
-display "_" as many times as the length in an array
-intake letters in a new method to test if they are in the word
-if the letter is in the word, replace the array of hashes at the spot that each letter shows up (make sure it is replacing a instances of the letter, not just the first one)
-if the letter is not in the word, show a list (array) of incorrect letters guessed
-do NOT penalize for repeat guesses
-let the user guess 2x as many times as there are letters in the word
-if all the _s in the array have been replaced by letters before the guesses are up, end the game and say it has been won
-if all guesses are used prior to finishing the array, say game over and print the word
-=end
-
 class Game
-
   def initialize(word)
-   @word = word.downcase
-   @guess_count = 0
+    @word = word.downcase
+    @length = word.length
+    @max_guesses = word.length * 2
+    @guess_count= 0
     @incorrect_guesses = []
     @correct_guesses = []
-    @max_guesses = word.length * 2
-    hashes = "_" * word.length
-    @word_array = hashes.split("")
-    puts hashes
   end
+
+  def hash
+    value = "_" * @length
+    return value
+  end
+
 
 end
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,7 +1,8 @@
 require_relative 'game'
+
 describe Game do
-  it "takes a word and returns a string of '_'s in place of each letter" do
-    game = Game.new("dog")
-    expect(game.create_hash).to eq "___"
+  it "takes the word from initialize and outputs a string with hashes in place of every character" do
+    game = Game.new('dog')
+    expect(game.hash).to eq "___"
   end
 end

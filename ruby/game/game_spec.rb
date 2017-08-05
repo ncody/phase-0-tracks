fcb7c2fc73930beab81c523e42d050e420764586
Nicole Cody <nicole.cody7@gmail.com>
2017-08-05 18:17:28 -0500

    Add comments on test code and pseudocode

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -7,12 +7,18 @@ commented out because this method has to be in initialize in order to make an ar
     game = Game.new('dog')
     expect(game.hash).to eq "___"
   end
+ commented out because you can't run test code on initialize
+  it "takes the word from initialize and outputs a string with hashes in place of every character" do
+    game = Game.new('dog')
+    expect(game.initiaize).to eq "___"
+  end
 =end
 
-  it "checks the guessed letter to see if it has already been guessed" do
-      game = Game.new('dog')
-      expect(game.check_guess('d')).to eq 1
-    end
+#commented out because was funcitioning when added but had to be added to the check word function below
+  #it "checks the guessed letter to see if it has already been guessed" do
+   #   game = Game.new('dog')
+    #  expect(game.check_guess('d')).to eq 1
+    #end
 
   it "checks the original word for the letter, if it has it it adds it in that index to the array of hashes, if not, it adds the letter to incorrect guesses" do
      game = Game.new('dog')

638a49f7f6b370fa71dcc8fa9b4207af15fc5c9d
Nicole Cody <nicole.cody7@gmail.com>
2017-08-09 12:40:01 -0500

    Add object combining array of names and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,18 @@
 var colors = ["blue", "green", "red", "yellow"];
 var names = ["Ed", "Secretariat", "Moon Shadow", "Buttercup" ];
 
-console.log(colors);
+//console.log(colors);
 colors.push("orange");
-console.log(colors);
+//console.log(colors);
 
-console.log(names);
+//console.log(names);
 names.push("Seabiscut");
-console.log(names);
+//console.log(names);
+
+
+var horses = {};
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);

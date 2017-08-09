00eefde3fa3df5c764bab13a6fe0001effaadf88
Nicole Cody <nicole.cody7@gmail.com>
2017-08-09 12:04:23 -0500

    Add array of colors and horses, and add to both arrays

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["blue", "green", "red", "yellow"];
+var names = ["Ed", "Secretariat", "Moon Shadow", "Buttercup" ];
+
+console.log(colors);
+colors.push("orange");
+console.log(colors);
+
+console.log(names);
+names.push("Seabiscut");
+console.log(names);

de931c6df7fe92a41fe32408d132b9cd137d0a10
Nicole Cody <nicole.cody7@gmail.com>
2017-08-21 13:13:16 -0500

    Add print list method

---
+++ databases/module8.5/groceries.rb
@@ -0,0 +1,63 @@
+=begin
+Create a database that stores grocery items and quantity wanted
+Create a method to add a new item with a quantity
+Let user enter as many items as wanted
+Print out the final grocery list
+=end
+
+require 'sqlite3'
+
+db = SQLite3::Database.new("groceries.db")
+db.results_as_hash = true
+
+create_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS groceries(
+    id INTEGER PRIMARY KEY,
+    item VARCHAR(255),
+    quantity INT
+  )
+SQL
+
+
+db.execute(create_table)
+#db.execute("INSERT INTO groceries (item, quantity) VALUES ('Granola Bars', 4)")
+
+def add_item(db, item, quantity)
+  db.execute("INSERT INTO groceries (item, quantity) VALUES (?, ?)", [item, quantity])
+end
+
+
+
+def print_list(db)
+  puts "Grocery list:"
+puts "--------------"
+groceries = db.execute("SELECT * FROM groceries")
+groceries.each do |grocery|
+  puts "#{grocery['item']}: #{grocery['quantity']}"
+end
+puts "--------------"
+end
+
+
+
+#add_item(db, "Watermelon", 1)
+#add_item(db, "Apples", 4)
+puts "Welcome to your grocery list! \nYour grocery list currently includes:"
+print_list(db)
+new_item = nil
+until new_item == "no" do
+puts "Would you like to add a new item?"
+new_item = gets.chomp.downcase
+  if new_item == "no"
+    break
+  else
+  puts "What is the item?"
+  item = gets.chomp.capitalize
+  puts "What is the quantity?"
+  quantity = gets.chomp.to_i
+  add_item(db, item, quantity)
+end
+end
+puts "Final Grocery List:"
+print_list(db)
+

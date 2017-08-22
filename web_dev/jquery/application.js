b879ac26de81739fde452a5fbf3625345d5e7d32
Nicole Cody <nicole.cody7@gmail.com>
2017-08-22 15:02:35 -0500

    Add result notes about research

--- web_dev/jquery/application.js
+++ web_dev/jquery/application.js
@@ -2,11 +2,13 @@ window.onload = function () {
   alert("Welcome to my page :)");
 }
 
-$('h1').text("Woo jQuery Practice!");
+$('h1').text("My jQuery Practice Page!");
+
+$('#change').css('background', 'pink');
 
 $(document).ready(function() {
   $('#change').on('click', function() {
-    var newWords= $('<p>Netflix, Google, Microsoft, etc.</p>');
+    var newWords= $('<p>Woo! The text changed.</p>');
     $('div').append(newWords);
     $('#change').remove();
 });

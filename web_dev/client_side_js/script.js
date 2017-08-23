fb81d99619a17e87af14b2a7a680f8b535ee50e7
Nicole Cody <nicole.cody7@gmail.com>
2017-08-23 12:17:01 -0500

    Remove redunant naming of variables

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,17 +1,16 @@
 console.log("Hello");
 
-var coffeeimage = document.getElementById("coffeeimage");
-coffeeimage.hidden = true;
-
 var headers = document.getElementsByTagName('h1');
 var header = headers[0];
 header.style.border = "3px solid purple";
 
-function addDrinkImages () {
- var coffee = document.getElementById('coffee');
- var coffeeimage = document.getElementById("coffeeimage");
-coffeeimage.hidden = false;
+var coffeeImage = document.getElementById("coffeeimage");
+
+coffeeImage.hidden = true;
+
+function addCoffeeImage () {
+  coffeeImage.hidden = false;
 }
 
 var coffee = document.getElementById('coffee');
-coffee.addEventListener("mouseover", addDrinkImages);
+coffee.addEventListener("mouseover", addCoffeeImage);

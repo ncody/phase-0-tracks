b8a708c3b287a3c3753282f0ecddba04d27dff81
Nicole Cody <nicole.cody7@gmail.com>
2017-08-23 12:26:50 -0500

    Add images when mouse is over the word for all drinks

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -14,3 +14,46 @@ function addCoffeeImage () {
 
 var coffee = document.getElementById('coffee');
 coffee.addEventListener("mouseover", addCoffeeImage);
+
+var espressoImage = document.getElementById("espressoimage");
+
+espressoImage.hidden = true;
+
+function addEspressoImage () {
+  espressoImage.hidden = false;
+}
+
+var espresso = document.getElementById('espresso');
+espresso.addEventListener("mouseover", addEspressoImage);
+var teaImage = document.getElementById("teaimage");
+
+teaImage.hidden = true;
+
+function addTeaImage () {
+  teaImage.hidden = false;
+}
+
+var tea = document.getElementById('tea');
+tea.addEventListener("mouseover", addTeaImage);
+
+var waterImage = document.getElementById("waterimage");
+
+waterImage.hidden = true;
+
+function addWaterImage () {
+  waterImage.hidden = false;
+}
+
+var water = document.getElementById('water');
+water.addEventListener("mouseover", addWaterImage);
+
+var kombuchaImage = document.getElementById("kombuchaimage");
+
+kombuchaImage.hidden = true;
+
+function addKombuchaImage () {
+  kombuchaImage.hidden = false;
+}
+
+var kombucha = document.getElementById('kombucha');
+kombucha.addEventListener("mouseover", addKombuchaImage);

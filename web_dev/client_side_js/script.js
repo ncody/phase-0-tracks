214808767d21af9d50f702d29a9c219253d84c80
Nicole Cody <nicole.cody7@gmail.com>
2017-08-23 12:05:19 -0500

    Add coffee picture when mouse hovers over the word Coffee

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,17 @@
 console.log("Hello");
 
+var coffeeimage = document.getElementById("coffeeimage");
+coffeeimage.hidden = true;
+
 var headers = document.getElementsByTagName('h1');
 var header = headers[0];
 header.style.border = "3px solid purple";
+
+function addDrinkImages () {
+ var coffee = document.getElementById('coffee');
+ var coffeeimage = document.getElementById("coffeeimage");
+coffeeimage.hidden = false;
+}
+
+var coffee = document.getElementById('coffee');
+coffee.addEventListener("mouseover", addDrinkImages);
